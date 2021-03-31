f37a2290bbd818aae757a493b4b44fde2d28065f
Nicolas Gast <nicolas.gast@inria.fr>
2021-03-31 10:28:46 +0200

    all functions integrated in a class (except simulation)

--- pair_approx_ode.py
+++ pair_approx_ode.py
@@ -5,10 +5,51 @@ The power of two choices on graphs: the pair-approximation is accurate?
 N Gast - ACM SIGMETRICS Performance Evaluation Review, 2015 
 
 Main functions:
-- mm1(rho, N)
+- mm1(rho, d=50)
+- twoChoiceTheory(rho, d=50)
+- 
 
 '''
 import numpy as np
+
+class NEIGHBOOR_TWO_CHOICE():
+    """
+    proxy class to call various functions to compare simulation and approximations.
+    """
+    def __init__(self, rho, d=50):
+        """
+        d (default=50) should not a priori be changed.
+        """
+        self.rho = rho
+        self.d = d
+
+    def mm1(self):
+        """
+        Return the no choice value
+        """
+        return mm1(self.rho, self.d)
+
+    def mean_field_approximation(self):
+        """
+        Returns the mean field approximation
+        """
+        return twoChoiceTheory(self.rho, self.d)
+
+    def simulation(self):
+        assert False, "not implemented"
+        pass
+
+    def pair_approximation(self, force_recompute=False, atol=1e-7):
+        """
+        Returns the pair approximation. This methods numerically integrate
+        an ODE corresponding to the fixed point.
+
+        Args:
+        - force_recompute: if False, will try to load a file from disk
+        - atol: absolute numerical error tolerated (when computing the fixed point of the ODE)
+        """
+        return stationnary_distribution_pairApprox(self.rho, self.d, force_recompute, atol)
+
 
 def mm1(rho, d=50):
     """
@@ -25,8 +66,6 @@ def mm1(rho, d=50):
     x = rho**np.arange(d+1)
     return -np.diff(x)
 
-print(mm1(0.7))
-
 def twoChoiceTheory(rho, d=50):
     """
     Returns the steady-state distribution of the mean field approx. of
@@ -36,83 +75,75 @@ def twoChoiceTheory(rho, d=50):
     x = rho ** (2**np.arange(d)-1)
     return -np.diff(x)
 
-print(twoChoiceTheory(0.9))
-
-def derivative_2neighboorChoice(y, rho, mu, N=50):
+def derivative_2neighboorChoice(y, rho, mu=1, d=50):
     """
     Returns the derivative for the pair-approximation.
 
     Here y[i,j]=percentage of queue i with neighboor j (as defined as in the paper).
     """
-    derivative = np.zeros( (N, N))
-    p = np.zeros(N)
+    derivative = np.zeros( (d, d))
+    p = np.zeros(d)
 
-    for i in range(N):
+    for i in range(d):
         if sum(y[i]) >0:
-            p[i] = (sum (y[i][i+1:N]) + y[i][i]/2) / sum(y[i])
-    for i in range(N):
-        for j in range(N):
+            p[i] = (sum (y[i][i+1:d]) + y[i][i]/2) / sum(y[i])
+    for i in range(d):
+        for j in range(d):
             if i>0:
                 derivative[i][j]   +=  rho*p[i-1]*y[i-1][j] - mu*y[i][j]
                 derivative[i-1][j] += -rho*p[i-1]*y[i-1][j] + mu*y[i][j]
-                if (i<=j):
+                if  i<=j:
                     derivative[i][j]   +=  rho*y[i-1][j]
                     derivative[i-1][j] += -rho*y[i-1][j]
-                elif(i-1==j):
+                elif i-1==j:
                     derivative[i][j]   +=  rho*y[i-1][j]/2
                     derivative[i-1][j] += -rho*y[i-1][j]/2
             if j>0:
                 derivative[i][j]   +=  rho*p[j-1]*y[i][j-1] - mu*y[i][j]
                 derivative[i][j-1] += -rho*p[j-1]*y[i][j-1] + mu*y[i][j]
-                if (j<=i):
+                if j<=i:
                     derivative[i][j]   +=  rho*y[i][j-1]
                     derivative[i][j-1] += -rho*y[i][j-1]
-                elif (i==j-1):
+                elif i==j-1:
                     derivative[i][j]   +=  rho*y[i][j-1]/2
                     derivative[i][j-1] += -rho*y[i][j-1]/2
     return derivative
 
 
+def stationnary_distribution_pairApprox(rho, d=50, force_recompute=False, atol=1e-7):
+    """
+    Returns the stationary distribution for the pair-approximation.
 
-def stationnary_distribution_pairApprox(rho, N):
-    filename = ''.join(['results/pair_approx_N',str(N),'_',str(rho)])
-    try:
-        x = loadtxt(filename)
-        print('we load the saved result')
-        return x[:,1]
-    except Exception as e:
-        print(e)
-        print('computation was not done before. We do it and save the results')
-    
-    y = array([[0.]*N]*N)
-    for i in range(0,10):
-        for j in range(0,10):
+    This functin is based on a numerical integration of the ODE.
+    """
+    filename = ''.join(['results/pair_approx_d',str(d),'_',str(rho)])
+    if not force_recompute:
+        try:
+            return np.loadtxt(filename)
+        except Exception as excep:
+            print(excep)
+            print('computation was not done before. We do it and save the results')
+
+    # We first initialize the ODE
+    y = np.zeros((d,d))
+    for i in range(0,d):
+        for j in range(0,d):
             y[i][j]  = (rho**i)*(rho**j)
-    y = y/sum(y)
-    h = 0.1
-    derivative = array([[0.]*N]*N)
-    x0 = -1
-    x5 = -1
+    y = y/np.sum(y)
+    h = 0.1 # Step of the ODE
+    old_y = np.zeros((d,d))
     T = 100
     conv = 1
-    while (abs(sum(y[0])-1+rho)>1e-5
-           or abs(x0-sum(y[0])) > 1e-5
-           or abs(x5-sum(y[5])) > 1e-7):
-        #print( abs(sum(y[0])-1+rho))
-        x0 = sum(y[0])
-        x5 = sum(y[5])
+    while abs(sum(y[0])-1+rho)>1e-5 or np.sum(np.abs(y-old_y)) > atol:
+        old_y = np.copy(y)
         for t in range(0,T):
-            derivative_2neighboorChoice(y,rho,1,N, derivative)
-            y  = y  + h * derivative
-        print('T=',T*conv,'x0=',x0,'x5=',x5)
+            y  += h * derivative_2neighboorChoice(y, rho, 1, d)
+        print('T=', T*conv, 'diff=', np.sum(np.abs(y-old_y)))
         conv += 1
-        # for i in range(0,10):
-        #     x[t,i] = sum(y[i])
-        #x[t] = sum(y[0])
-    x = array([[0.,0.]]*N)
-    for i in range(0,N):
-        x[i,0] = i
-        x[i,1] = sum(y[i])
-    savetxt(filename, x)
-    return (x[:,1])
+    x = np.sum(y, 1)
+    np.savetxt(filename, x)
+    return x
 
+
+
+#print(stationnary_distribution_pairApprox(0.5, force_recompute=True))
